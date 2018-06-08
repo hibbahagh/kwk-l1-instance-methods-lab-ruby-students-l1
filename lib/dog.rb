bc6556733e6bfcc0db503125b37a055dfe88ea50
hibbahagh <hibbahagh@gmail.com>
2018-06-08 19:34:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # dog.rb
+
+class Dog
+  
+  def initialize
+    @bark = "Woof!"
+    @sit = "The dog is sitting."
+  end
+  
+  def bark
+    "#{@bark}"
+  end
+  
+  def sit 
+    "#{@sit}"
+  end 
+  
+end
+
+
+dog = Dog.new
+puts dog.bark()
+puts dog.sit()
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # person.rb
+
+class Person
+  def initialize
+    @talk = "Hello World!"
+    @walk = "The person is walking."
+  end
+  
+  def talk
+    "#{@talk}"
+  end
+  
+  def walk
+    "#{@walk}"
+  end
+  
+end
+
+person = Person.new
+puts person.talk()
+puts person.walk()
